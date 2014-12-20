a6f7d28cca492d6d784c703c49af167eeb932d09
Simon J.K. Pedersen <mail@sjkp.dk>
2014-12-20 07:36:03 +0100

    Init

---
+++ SJKP.ShortCode/ShortCode.fs
@@ -0,0 +1,44 @@
+﻿namespace SJKP.ShortCode
+
+module ShortCode = 
+    open System
+    let r = new System.Random(Guid.NewGuid().GetHashCode())
+    
+    let urlSafeString (s : string) = 
+        s.Replace('+','-').Replace('/','_').Replace("=","") 
+
+    let private toBinary (x : int) = 
+        System.Convert.ToString(x,2)
+    
+    let private fromBinary (x : string) = 
+        System.Convert.ToInt32(x, 2)
+
+
+    let private randomString(len : int) =        
+        let a = Array.init len (fun i -> byte(i))
+        r.NextBytes(a)
+        a
+
+    let private getDatePart(d : DateTime) = 
+        let b = toBinary(Convert.ToInt32(d.ToString("yy"))).PadLeft(7,'0') + toBinary(d.Month).PadLeft(4,'0') + toBinary(d.Day).PadLeft(5,'0')
+        let arr = [|Convert.ToByte(fromBinary(b.Substring(0,8))); Convert.ToByte(fromBinary(b.Substring(8)))|]
+        (arr |> Convert.ToBase64String)
+
+    let public NewShortCodeByDate(d : DateTime) = 
+        getDatePart(d) + (randomString(5) |> Convert.ToBase64String) |> urlSafeString
+    
+    let public NewShortCode() = 
+        let d = DateTime.UtcNow
+        NewShortCodeByDate(d)
+    
+    let public GetDate(s: string) =
+        let dpart = s.Replace('-','+').Replace('_','/').Substring(0,3) + "="
+        let b = Convert.FromBase64String(dpart)
+        let bstring = Convert.ToString(b.[0],2).PadLeft(8,'0') + Convert.ToString(b.[1],2).PadLeft(8,'0')
+        let year = 2000+fromBinary(bstring.Substring(0,7))
+        let month = fromBinary(bstring.Substring(7,4))
+        let day = fromBinary(bstring.Substring(11))
+        new DateTime(year,month,day)
+
+    let public ConvertToDatePart(d : DateTime) = 
+        getDatePart(d) |> urlSafeString
---
+++ SJKP.ShortCode/Tests.fsx
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#load "ShortCode.fs"
+open SJKP.ShortCode
+open System
+
+
+let d = new DateTime(2001,2,28)
+let dpart = ShortCode.ConvertToDatePart(d)
+let res = ShortCode.NewShortCodeByDate(d)
+let res3 = ShortCode.NewShortCodeByDate(d)
+let res1 = ShortCode.GetDate(res)
